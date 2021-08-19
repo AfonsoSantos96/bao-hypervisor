816006708e063f8b069d5c8f5ee9629b998c6d57
Aras Ashraf Gandomi <aras.gandomi@gmail.com>
2021-08-19 16:24:27 +0200

    refactor(portability): Added arch-specific types' headers placeholders
    To enable code portability, it's necessary to define arch-specific types.
    The added headers are placeholders.
    
    Signed-off-by: Aras Ashraf Gandomi <aras.gandomi@gmail.com>

---
+++ src/arch/armv8/inc/arch/types.h
@@ -0,0 +1,21 @@
+/**
+ * Bao, a Lightweight Static Partitioning Hypervisor
+ *
+ * Copyright (c) Bao Project (www.bao-project.org), 2021-
+ *
+ * Authors:
+ *      Aras Ashraf Gandomi <aras.ashrafgandomi2@unibo.it>
+ *
+ * Bao is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 2 as published by the Free
+ * Software Foundation, with a special exception exempting guest code from such
+ * license. See the COPYING file in the top-level directory for details.
+ *
+ */
+
+#ifndef __ARCH_TYPES_H__
+#define __ARCH_TYPES_H__
+
+#include <bao.h>
+
+#endif /* __ARCH_TYPES_H__ */
