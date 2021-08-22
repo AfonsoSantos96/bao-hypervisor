4eb90741cfa7e79e6d470f203a03d93aa1213d96
Aras Ashraf Gandomi <aras.gandomi@gmail.com>
2021-08-22 15:51:53 +0200

    docs(merge): Added notes for merging
    Signed-off-by: Aras Ashraf Gandomi <aras.gandomi@gmail.com>

--- src/arch/armv8/inc/arch/types.h
+++ src/arch/armv8/inc/arch/types.h
@@ -11,6 +11,8 @@
  * Software Foundation, with a special exception exempting guest code from such
  * license. See the COPYING file in the top-level directory for details.
  *
+ * DO NOT MERGE THIS FILE
+ * Refactor it into the appropriate headers!
  */
 
 #ifndef __ARCH_TYPES_H__
@@ -19,12 +21,14 @@
 #include <stdint.h>
 
 #define HYP_CTX_ID  0
+typedef uint64_t ctx_id_t;
 
 typedef uint64_t phys_addr_t;
-typedef uint64_t ctx_id_t;
+
 typedef uint64_t pt_lvl_t;
 typedef uint64_t pte_flag_t;
 typedef uint64_t pte_type_t;
+
 typedef uint64_t cpuid_t;
 
 #endif /* __ARCH_TYPES_H__ */
--- src/core/inc/types.h
+++ src/core/inc/types.h
@@ -1,3 +1,19 @@
+/**
+ * Bao, a Lightweight Static Partitioning Hypervisor
+ *
+ * Copyright (c) Bao Project (www.bao-project.org), 2021-
+ *
+ * Authors:
+ *      Aras Ashraf Gandomi <aras.ashrafgandomi2@unibo.it>
+ *
+ * Bao is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 2 as published by the Free
+ * Software Foundation, with a special exception exempting guest code from such
+ * license. See the COPYING file in the top-level directory for details.
+ * 
+ * DO NOT MERGE THIS FILE
+ * Refactor it into the appropriate headers!
+ */
 
 #ifndef __TYPES_H__
 #define __TYPES_H__
