9e1987e9d867a7166be6bffbb9795d84a34fb99e
Jose Martins <josemartins90@gmail.com>
2021-09-08 10:18:10 +0100

    make types size arch dependent

--- src/core/inc/types.h
+++ src/core/inc/types.h
@@ -22,16 +22,16 @@
 
 typedef signed long ssize_t;
 
-#define HYP_ASID  0
-typedef uint64_t asid_t;
-typedef uint64_t vmid_t;
 
-typedef uint64_t phys_addr_t;
-typedef uint64_t virt_addr_t;
+typedef unsigned long asid_t;
+typedef unsigned long vmid_t;
 
-typedef uint64_t colormap_t;
+typedef uintptr_t phys_addr_t;
+typedef uintptr_t virt_addr_t;
 
-typedef uint64_t cpuid_t;
+typedef unsigned long colormap_t;
+
+typedef unsigned long cpuid_t;
 typedef unsigned long cpumap_t;
 
 #endif
