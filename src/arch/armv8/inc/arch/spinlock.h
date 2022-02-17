908ff07ad8b792126d2894ffd74d605c4452ce99
AfonsoSantos96 <afomms@gmail.com>
2022-02-17 16:54:27 +0000

    Spinlock functions with aarch32 compatibility

--- src/arch/armv8/inc/arch/spinlock.h
+++ src/arch/armv8/inc/arch/spinlock.h
@@ -29,23 +29,25 @@ typedef volatile uint32_t spinlock_t;
 
 static inline void spin_lock(spinlock_t* lock)
 {
-/*   spinlock_t const ONE = 1;
+    spinlock_t const ONE = 1;
     spinlock_t tmp;
 
     asm volatile(
         "1:\n\t"
-        "ldaxr %r0, %1 \n\t"
-        "cbnz %r0, 1b \n\t"
-        "stxr %r0, %r2, %1 \n\t"
-        "cbnz %r0, 1b \n\t"
+        "ldrex %r0, %1 \n\t"
+        "cmp %r0, #0 \n\t"
+        "bne 1b \n\t"
+        "strex %r0, %r2, %1 \n\t"
+        "cmp %r0, #0 \n\t"
+        "bne 1b \n\t"
         : "=&r"(tmp), "+Q"(*lock)
         : "r"(ONE));
- */
 }
 
 static inline void spin_unlock(spinlock_t* lock)
 {
-    //asm volatile("stlr wzr, %0\n\t" ::"Q"(*lock));
+    spinlock_t const ZERO = 0;
+    asm volatile("str %r0, %1\n\t" ::"r"(ZERO), "Q"(*lock));
 }
 
 #endif /* __ARCH_SPINLOCK__ */
