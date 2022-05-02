6db4aaac7a332ed98dc94e50ec291090fb3a56ad
AfonsoSantos96 <afomms@gmail.com>
2022-05-02 17:20:59 +0100

    add aarch32 vm profile registers

--- src/arch/armv8/armv8-r/vm.c
+++ src/arch/armv8/armv8-r/vm.c
@@ -22,7 +22,7 @@ void vcpu_arch_profile_init(struct vcpu* vcpu, struct vm* vm) {
 }
 
 void vcpu_arch_profile_reset(struct vcpu* vcpu) {
-    vcpu->regs.spsr_hyp = SPSR_USR | SPSR_F | SPSR_I | SPSR_A;
+    vcpu->regs.spsr_hyp = SPSR_SVC | SPSR_F | SPSR_I | SPSR_A;
 }
 
 bool vcpu_arch_profile_on(struct vcpu* vcpu) {
--- src/arch/armv8/inc/arch/sysregs.h
+++ src/arch/armv8/inc/arch/sysregs.h
@@ -59,8 +59,12 @@
 #define SPSR_IL (1 << 20)
 #define SPSR_SS (1 << 21)
 
-#define SPSR_USR (0x0)
-#define SPSR_SYS (0xf)
+#define SPSR_USR (0x10)
+#define SPSR_IRQ (0x12)
+#define SPSR_SVC (0x13)
+#define SPSR_ABT (0x17)
+#define SPSR_UND (0x1b)
+#define SPSR_SYS (0x1f)
 
 /* SCR - Secure Configuration Register */
 
