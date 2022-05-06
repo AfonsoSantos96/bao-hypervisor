aaca98856db7fc824443108cd1d80fcd739debbf
AfonsoSantos96 <afomms@gmail.com>
2022-05-06 19:56:40 +0100

    wrap alloc and map of vpage into a single function on aarch32

--- src/core/mpu/mem.c
+++ src/core/mpu/mem.c
@@ -1,6 +1,7 @@
 #include <mem.h>
 
 #include <cpu.h>
+#include <bao.h>
 
 void mem_prot_init() {
 
@@ -21,4 +22,14 @@ bool mem_map(struct addr_space *as, vaddr_t va, struct ppages *ppages,
             size_t n, mem_flags_t flags)
 {
     return false;
+}
+
+vaddr_t mem_alloc_map (struct addr_space* as, enum AS_SEC section, struct ppages *page, 
+                        vaddr_t at, size_t size, mem_flags_t flags)
+{
+    vaddr_t address = mem_alloc_vpage (as, section, at, size);
+    if (address != at) ERROR("Can't allocate address");
+    mem_map(as, address, page, size, flags);
+
+    return address;
 }
