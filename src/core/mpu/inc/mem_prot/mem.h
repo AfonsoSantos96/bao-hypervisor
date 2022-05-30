430813a60daa53efd60e4a06ee04dacd613a86ca
AfonsoSantos96 <afomms@gmail.com>
2022-05-30 11:54:15 +0100

    add as init declaration for mpu

--- src/core/mpu/inc/mem_prot/mem.h
+++ src/core/mpu/inc/mem_prot/mem.h
@@ -5,6 +5,7 @@
  *
  * Authors:
  *      Jose Martins <jose.martins@bao-project.org>
+ *      Afonso Santos <afomms@gmail.com>
  *
  * Bao is free software; you can redistribute it and/or modify it under the
  * terms of the GNU General Public License version 2 as published by the Free
@@ -21,11 +22,15 @@
 
 enum AS_TYPE { AS_HYP = 0, AS_VM, AS_HYP_CPY };
 
+#define HYP_ASID  0
 struct addr_space {
     enum AS_TYPE type;
     colormap_t colors;
 };
 
 typedef unsigned long long mem_flags_t;
+
+void as_init(struct addr_space* as, enum AS_TYPE type,
+            colormap_t colors);
 
 #endif /* __MEM_PROT_H__ */
