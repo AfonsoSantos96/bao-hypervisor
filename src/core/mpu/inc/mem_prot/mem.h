2dcc39896bc8ff093aa4811b061786dc7051eecb
AfonsoSantos96 <afomms@gmail.com>
2022-05-30 11:54:15 +0100

    add as init declaration for mpu

--- src/core/mpu/inc/mem_prot/mem.h
+++ src/core/mpu/inc/mem_prot/mem.h
@@ -11,11 +11,15 @@
 
 enum AS_TYPE { AS_HYP = 0, AS_VM, AS_HYP_CPY };
 
+#define HYP_ASID  0
 struct addr_space {
     enum AS_TYPE type;
     colormap_t colors;
 };
 
 typedef unsigned long long mem_flags_t;
+
+void as_init(struct addr_space* as, enum AS_TYPE type,
+            colormap_t colors);
 
 #endif /* __MEM_PROT_H__ */
