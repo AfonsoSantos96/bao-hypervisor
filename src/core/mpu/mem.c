0851d62a3eadf7e4c7a55d38fea5cd9b1d2a7fa4
AfonsoSantos96 <afomms@gmail.com>
2022-05-30 12:13:38 +0100

    define mem_map function and declare mem_set_region_mpu

--- src/core/mpu/mem.c
+++ src/core/mpu/mem.c
@@ -5,21 +5,20 @@
 
 #include <mem.h>
 #include <cpu.h>
+#include <bao.h>
+#include <fences.h>
+
+extern bool mem_reserve_ppages(struct ppages *ppages);
+extern bool mem_reserve_ppool_ppages(struct page_pool *pool, struct ppages *ppages);
+extern bool mem_are_ppages_reserved(struct ppages *ppages);
 
 void mem_prot_init() {
     as_init(&cpu()->as, AS_HYP, 0);
 }
 
-
 size_t mem_cpu_boot_alloc_size() {
     size_t size = ALIGN(sizeof(struct cpu), PAGE_SIZE);
     return size;
-}
-
-bool mem_map(struct addr_space *as, vaddr_t va, struct ppages *ppages,
-            size_t n, mem_flags_t flags)
-{
-    return false;
 }
 
 void as_init(struct addr_space *as, enum AS_TYPE type, 
@@ -27,6 +26,35 @@ void as_init(struct addr_space *as, enum AS_TYPE type,
 {
     as->type = type;
     as->colors = 0;
+}
+
+bool mem_set_region_mpu(vaddr_t va, size_t n, mem_flags_t flags)
+{
+    /* Search for a free slot region */
+    /* Fill mpu registers */
+    return true;
+}
+
+bool mem_map(struct addr_space *as, vaddr_t va, struct ppages *ppages,
+            size_t n, mem_flags_t flags)
+{
+        /* Address does not belong to a device/shared memory region */
+        /* TODO: Redefine PTE_HYP_DEV_FLAGS value */
+    if ((flags != PTE_HYP_DEV_FLAGS) && !mem_are_ppages_reserved(ppages)){
+        mem_reserve_ppages(ppages);
+        if (!mem_set_region_mpu(va, n, flags)){ 
+            ERROR ("Error setting mpu region for %x addr", va);
+        }
+        fence_sync();
+    } else if ((flags == PTE_HYP_DEV_FLAGS)){
+        if (!mem_set_region_mpu(va, n, flags)){
+            ERROR ("Error setting mpu region for device at %x addr", va);
+        }
+    } else {
+        ERROR ("Address already allocated!");
+    }
+
+    return true;
 }
 
 vaddr_t mem_alloc_map(struct addr_space* as, enum AS_SEC section, struct ppages *page,
