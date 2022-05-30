6baa4b5abeef7f5906a25e377179d0db07c13973
AfonsoSantos96 <afomms@gmail.com>
2022-05-30 12:02:26 +0100

    define mem_alloc_map and mem_alloc_map_dev

--- src/core/mpu/mem.c
+++ src/core/mpu/mem.c
@@ -24,13 +24,26 @@ bool mem_map(struct addr_space *as, vaddr_t va, struct ppages *ppages,
 vaddr_t mem_alloc_map(struct addr_space* as, enum AS_SEC section, struct ppages *page,
                         vaddr_t at, size_t size, mem_flags_t flags)
 {
-    return at;
+    // TODO: Check if page->base, page->size and vaddr_t at are page_size align?
+    vaddr_t address = NULL_VA;
+    if (page == NULL){
+        struct ppages temp_page = mem_ppages_get(at, size);
+        address = mem_map(as, at, &temp_page, size, flags);
+    }
+    else {
+        address = mem_map(as, at, page, size, flags);
+    }
+
+    return address;
 }
 
 vaddr_t mem_alloc_map_dev(struct addr_space* as, enum AS_SEC section,
                              vaddr_t at, paddr_t pa, size_t size)
 {
-    return pa;
+    mem_flags_t flags = PTE_HYP_DEV_FLAGS;
+    struct ppages temp_page = mem_ppages_get(at, size);
+    vaddr_t address = mem_map(as, at, &temp_page, size, flags);
+    return address;
 }
 
 vaddr_t mem_map_cpy(struct addr_space *ass, struct addr_space *asd, vaddr_t vas,
