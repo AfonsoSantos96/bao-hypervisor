e2f758d6b26802ed66f766ccac50e96b19edc330
AfonsoSantos96 <afomms@gmail.com>
2022-08-07 23:43:44 +0100

    enable interrupt controller before memory init to send ipi

--- src/arch/armv8/armv8-r/cpu.c
+++ src/arch/armv8/armv8-r/cpu.c
@@ -5,9 +5,11 @@
 
 #include <cpu.h>
 #include <platform.h>
+#include <arch/gic.h>
 
 void cpu_arch_profile_init(cpuid_t cpuid, paddr_t load_addr) {
-
+    /*  Enable Interrupt Controller to send ipi during memory initialization */
+    sysreg_icc_sre_el2_write(ICC_SRE_SRE_BIT | ICC_SRE_ENB_BIT);
 }
 
 void cpu_mem_prot_bitmap_init(struct cpu_arch_profile* mp)
