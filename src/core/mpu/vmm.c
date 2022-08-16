7d533ba36aae8ed883d4defa8627efe7acf6bb7d
AfonsoSantos96 <afomms@gmail.com>
2022-08-16 16:01:04 +0100

    implement vmm/vm install flow

--- src/core/mpu/vmm.c
+++ src/core/mpu/vmm.c
@@ -6,14 +6,20 @@
 #include <vmm.h>
 #include <io.h>
 
+extern void mem_set_shared_region(unsigned long as, vaddr_t va, size_t n, 
+                            mem_flags_t flags);
+
 void vmm_io_init() {
 
 }
 
 struct vm_install_info vmm_get_vm_install_info(struct vm* vm) {
-    return (struct vm_install_info){};
+    return (struct vm_install_info){.base_addr = (vaddr_t)vm, .size = sizeof(*vm)};
 }
 
 void vmm_vm_install(struct vm* vm, struct vm_install_info *install_info) {
+    /*  Define "0" */
+    mem_set_shared_region(0, install_info->base_addr, install_info->size, 
+                            0x100);//PTE_VM_FLAGS);
 
 }
