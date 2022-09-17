c3c8e3f701082849bdb7ae55bf7dde81102c831a
AfonsoSantos96 <afomms@gmail.com>
2022-09-17 09:22:58 +0100

    change vmm_get_vm_install_info

--- src/core/mpu/vmm.c
+++ src/core/mpu/vmm.c
@@ -13,13 +13,15 @@ void vmm_io_init() {
 
 }
 
-struct vm_install_info vmm_get_vm_install_info(struct vm* vm) {
-    return (struct vm_install_info){.base_addr = (vaddr_t)vm, .size = sizeof(*vm)};
+struct vm_install_info vmm_get_vm_install_info(struct vm* vm, size_t ncpus) {
+    return (struct vm_install_info){.base_addr = (vaddr_t)vm, 
+                                    .size = sizeof(*vm) + 
+                                            (ncpus * sizeof(struct vcpu)) };
 }
 
 void vmm_vm_install(struct vm* vm, struct vm_install_info *install_info) {
     /*  Define "0" */
     mem_set_shared_region(0, install_info->base_addr, install_info->size, 
-                            0x100);//PTE_VM_FLAGS);
+                            PTE_VM_FLAGS);
 
 }
